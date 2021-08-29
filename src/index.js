3ce4cfa66cd7914631588029f064660209a3bceb
Anastasia Romanenko <romane.anastasia@gmail.com>
2021-08-29 21:34:41 +0300

    д/з - 10

---
+++ src/index.js
@@ -0,0 +1,13 @@
+ import items from "./menu.json";
+ import menuTemplate from "./templates/list.hbs";
+ import "./styles.css";
+
+ console.log(items)
+ console.log(menuTemplate)
+
+ const list = document.querySelector('.js-menu');
+
+//   const markup = items.map(menuTemplate).join('');
+  const markup = menuTemplate(items);
+  console.log(markup);
+  list.insertAdjacentHTML('afterbegin', markup);
---
+++ src/localstorage.js
@@ -0,0 +1,55 @@
+import "./styles.css";
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const themeSwitch = document.querySelector('body');
+const checkbox = document.querySelector('#theme-switch-toggle');
+ 
+checkbox.addEventListener('change', onThemeSwitchInput);
+
+
+  // const dataForStorage = JSON.stringify(Theme);
+  // console.log(dataForStorage);
+  // localStorage.setItem('Theme', dataForStorage);
+
+  // const storageCollection = JSON.parse(localStorage.getItem('Theme'));
+  //const dataFromStorage = storageCollection ? storageCollection : document.body.classList.add("light-theme");
+  
+
+const savedTheme = localStorage.getItem('Theme');
+
+saveTheme ();
+  function saveTheme () {
+    
+ if (savedTheme === null) {
+    document.body.classList.add("light-theme");
+    localStorage.setItem("Theme", "light-theme");
+  }
+  }
+  
+   
+  
+  if (savedTheme === Theme.DARK) {
+    checkbox.setAttribute('checked', 'checked');
+    document.body.classList.remove("light-theme");
+    document.body.classList.add('dark-theme');
+    localStorage.setItem("Theme", "dark-theme");
+  }
+
+
+
+  function onThemeSwitchInput (e) {
+      if  (checkbox.checked) {
+       document.body.classList.remove("light-theme");
+       document.body.classList.add("dark-theme"); 
+       localStorage.setItem("Theme", "dark-theme");
+       return
+      }
+      document.body.classList.remove("dark-theme");
+      document.body.classList.add("light-theme"); 
+      localStorage.setItem("Theme", "light-theme");
+    }
+        
